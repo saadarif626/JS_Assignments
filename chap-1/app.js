44e74b17ec223a65420be1cffa1a17131f2a28e2
saadarif626 <saadarif626@gmail.com>
2024-09-21 12:00:32 +0500

    Initial commit

---
+++ chap-1/app.js
@@ -0,0 +1,22 @@
+         // Question-1
+
+// {alert(" welcome to the Website")}
+
+//         //  Question-2
+
+// {alert(" Error! Please enter valid password")} 
+
+        //  Question-3
+
+//  {alert(" Welcome to JS land.... \n Happy coding")}
+
+         //  Question-4
+
+//  {alert(" Welcome to JS land....")}
+ {alert(" Happy Coding \n Prevent this page from creating additional dialogue")}
+
+            // Question-5
+
+{alert("Hello.... I can run JS trough my browser's console")}
+
+
---
+++ chap-4/app.js
@@ -0,0 +1,33 @@
+        //question-1
+var name="saad"
+var batch="batch12"
+var loca="SMIT"
+document.write("Myself"+"  "+ name +" " +"part of" +"  "+ loca +"  " + " " + "in"+ batch)
+
+
+        // question-2
+
+        //   legal var
+var $money=
+var _name=
+var firstName=
+var off_user=
+var user123=
+
+//        // Illegal var
+
+var 1stName=
+var user-name=
+var my name=
+var function=
+var @amount=
+
+document.write("<h1>" + "Rules for naming JS variables" + "<br/> <br/>")
+
+document.write("Variable names can only contain number, $ and _. For example $my_1stVariable." +"<br/> ")
+
+document.write("Variables must begin with a Letter, $ or _. For example $name, _name or name." +"<br/>")
+
+document.write("Variable names are case Sentitive." + "<br/>")
+
+document.write("Variable names should not be JS Keywords." + "<br/> ")
